983e453c6fc86fe40bc94a306a2ca952f1fb7afc
ian-codes <72500207+ian-codes@users.noreply.github.com>
2023-07-13 18:23:51 +0200

    Add files via upload

---
+++ _app/immutable/chunks/paths.ea9bf7d2.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_18oase3)==null?void 0:s.base)??"/connectbern.github.io";var e;const t=((e=globalThis.__sveltekit_18oase3)==null?void 0:e.assets)??a;export{t as a,a as b};
