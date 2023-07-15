77e6a196c7f7c088ee5c0df2b078da38e5301a22
ian-codes <72500207+ian-codes@users.noreply.github.com>
2023-07-15 10:16:02 +0200

    Try to fix bg scroll bounce

---
+++ _app/immutable/chunks/paths.e11ed344.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_a5kqwg)==null?void 0:s.base)??"/connectbern";var a;const t=((a=globalThis.__sveltekit_a5kqwg)==null?void 0:a.assets)??e;export{t as a,e as b};
