2593ac1101d18b14d7c7c12c6568d166ad7d22d2
Ian <sugarrush@keanu.home>
2023-07-16 17:26:21 +0200

    Updates

--- _app/immutable/chunks/paths.ba5eabd0.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1i341bk)==null?void 0:s.base)??"/connectbern.github.io";var e;const a=((e=globalThis.__sveltekit_1i341bk)==null?void 0:e.assets)??t;export{a,t as b};
