76a63393f46f4fda89ac63cf23040b1d2b6a05de
Ian <sugarrush@keanu.home>
2023-07-16 17:53:43 +0200

    remove unnecessary directories

--- .svelte-kit/output/client/_app/immutable/nodes/3.6236fea4.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.b95eede2.js";import{S as t,i as e}from"../chunks/index.619ddb4d.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.8c0281cb.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.619ddb4d.js","_app/immutable/chunks/each.e59479a4.js"];
-export const stylesheets = ["_app/immutable/assets/2.3e9b8866.css"];
-export const fonts = [];
