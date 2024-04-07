00c2904fdab7350808d8bff87fe0ca9caaa88d2a
bkrafi <bkrafi@bbcag.ch>
2024-04-07 17:05:11 +0200

    add redirect from google group links to new /groups/slug link instead

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Home • Connect Bern </title>
+</svelte:head>
+
+
+<script>
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+
+    export let data;
+
+    onMount(() => {
+        goto("/groups/" + data.slug);
+    })
+</script>
