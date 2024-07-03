eea5f9593228e917c30b73a6e0aa274b5a339ac6
ian-codes <ian.kraft@hotmail.com>
2024-07-03 08:49:05 +0200

    fix: h1 on mobile; improve new page

--- src/routes/finance/+page.svelte
+++ src/routes/finance/+page.svelte
@@ -1,26 +1,27 @@
-<h1>
-    Finance
-</h1>
-
-<ul>
-    <li>
-        <p>
-            Ticket discounts (museums, concerts) if you have Mobiliar, Sunrise or Raiffeisen
-        </p>
-    </li>
-    <li>
-        <p>
-            Nice deals in your Twint app
-        </p>
-    </li>
-    <li>
-        <p>
-            ZKB has sometimes discounts
-        </p>
-    </li>
-    <li>
-        <p>
-            TCS has discounts
-        </p>
-    </li>
-</ul>
+<section>
+    <div class="topbarWrapper">
+        <h1>Finance</h1>
+    </div>
+    <ul>
+        <li>
+            <p>
+                Ticket discounts (museums, concerts) if you have Mobiliar, Sunrise or Raiffeisen
+            </p>
+        </li>
+        <li>
+            <p>
+                Nice deals in your Twint app
+            </p>
+        </li>
+        <li>
+            <p>
+                ZKB has sometimes discounts
+            </p>
+        </li>
+        <li>
+            <p>
+                TCS has discounts
+            </p>
+        </li>
+    </ul>
+</section>
