f21d215dcfaaa57efa3ddd08eb5798478b618ec5
Chagai Friedlander <chagai.friedlander@fairkom.eu>
2025-11-06 14:52:59 +0100

    try to fix not building

--- src/routes/events/[slug]/+page.js
+++ src/routes/events/[slug]/+page.js
@@ -1,5 +1,13 @@
 import { events } from "$lib/components/calendar/events.js";
 
+export const prerender = true;
+
+export function entries() {
+    return events
+        .filter(e => e.extendedProps?.slug)
+        .map(e => ({ slug: e.extendedProps.slug }));
+}
+
 export function load({ params }) {
     return events.find(e => e.extendedProps.slug === params.slug);
 }
