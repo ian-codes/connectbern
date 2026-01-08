7e28897f2ee9f3204b4bb32012e3bb6898d07569
Chagai Friedlander <chagai.friedlander@fairkom.eu>
2026-01-08 17:23:41 +0100

    change default to be all events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -13,7 +13,7 @@
 
     let eventsDisplayMode = 'events'; // 'events' or 'resources'
     let calendarEvents = [];
-    let filterMode = 'connectbern'; // 'all' or 'connectbern'
+    let filterMode = 'all'; // 'all' or 'connectbern'
 
     // Simple chronological event list
     $: filteredEvents = (() => {
