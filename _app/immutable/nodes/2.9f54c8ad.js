084c5f464e136727e0b7b44b106bc64a61b8921f
chagai95 <chagai95@users.noreply.github.com>
2026-01-12 14:47:35 +0000

    deploy: bbcfaca340326b170cb8f8023c209f7d1f79d7d0

---
+++ _app/immutable/nodes/2.9f54c8ad.js
@@ -0,0 +1,1 @@
+import{s as n,o as s}from"../chunks/scheduler.ac9644d6.js";import{S as r,i as e}from"../chunks/index.2883b4b7.js";import{g as a}from"../chunks/navigation.d3c41aca.js";function i(o){return s(()=>{a("/")}),[]}class c extends r{constructor(t){super(),e(this,t,i,null,n,{})}}export{c as component};
