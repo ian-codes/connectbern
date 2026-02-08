361227afe8771f482750418149a115402939b0c0
chagai95 <chagai95@users.noreply.github.com>
2026-02-08 19:28:28 +0000

    deploy: 5d5ded1534d6a37233b8de571877a7edb84dae24

--- _app/immutable/chunks/navigation.e5347ad8.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.504d5fab.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/paths.a467ce57.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_43q8tx)==null?void 0:s.base)??"";var t;const e=((t=globalThis.__sveltekit_43q8tx)==null?void 0:t.assets)??a;export{e as a,a as b};
