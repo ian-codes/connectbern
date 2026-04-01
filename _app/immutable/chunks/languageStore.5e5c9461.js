a0304f387a53e339c3819404cf736754184c0731
chagai95 <chagai95@users.noreply.github.com>
2026-04-01 15:52:22 +0000

    deploy: 9ebeefe7f43d1e7ae44cae20699c35b10f1009fa

---
+++ _app/immutable/chunks/languageStore.5e5c9461.js
@@ -0,0 +1,1 @@
+import{w as r}from"./index.c7932760.js";function o(){const{subscribe:t,set:n,update:a}=r("en");return{subscribe:t,set:e=>{typeof window<"u"&&localStorage.setItem("language",e),n(e)},update:a}}const c=o();export{c};
---
+++ _app/immutable/chunks/navigation.99f1f4a2.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.a1e97e5c.js";const e=o("goto");export{e as g};
