56b7c78b26121b43a9aeac342d02607da98b0693
chagai95 <31655082+chagai95@users.noreply.github.com>
2026-07-03 12:48:39 +0200

    Zeit anpassen berndütsch (events.js)

--- src/lib/components/calendar/events.js
+++ src/lib/components/calendar/events.js
@@ -1,62 +1,60 @@
 
-
 let start = new Date(2024, 3, 6, 8, 0);
 let end = new Date(2024, 3, 6, 10, 0);
 
-
 export const events = [
     {
-        title: "BlaBla Language Exchange",
-        start: new Date(2024, 3, 7, 19, 0),
-        end: new Date(2024, 3, 7, 22, 0),
-        extendedProps: { slug: "blabla-language-exchange" }
+                title: "BlaBla Language Exchange",
+                start: new Date(2024, 3, 7, 19, 0),
+                end: new Date(2024, 3, 7, 22, 0),
+                extendedProps: { slug: "blabla-language-exchange" }
     },
     {
-        title: "Karaoke Night at Delfino",
-        start: new Date(2024, 3, 5, 21, 0),
-        end: new Date(2024, 3, 5, 23, 0),
-        extendedProps: { slug: "karaoke-night-delfino" }
+                title: "Karaoke Night at Delfino",
+                start: new Date(2024, 3, 5, 21, 0),
+                end: new Date(2024, 3, 5, 23, 0),
+                extendedProps: { slug: "karaoke-night-delfino" }
     },
     {
-        title: "Meetup",
-        start: start,
-        end: end,
-        extendedProps: { slug: "meetup" }
+                title: "Meetup",
+                start: start,
+                end: end,
+                extendedProps: { slug: "meetup" }
     },
     {
-        title: "Chess Night",
-        start: new Date(2025, 10, 4, 18, 0),
-        end: new Date(2025, 10, 4, 21, 0),
-        extendedProps: { slug: "chess" }
+                title: "Chess Night",
+                start: new Date(2025, 10, 4, 18, 0),
+                end: new Date(2025, 10, 4, 21, 0),
+                extendedProps: { slug: "chess" }
     },
     {
-        title: "Connect & Cheers",
-        start: new Date(2025, 10, 7, 20, 0),
-        end: new Date(2025, 10, 7, 22, 0),
-        extendedProps: { slug: "connect-and-cheers" }
+                title: "Connect & Cheers",
+                start: new Date(2025, 10, 7, 20, 0),
+                end: new Date(2025, 10, 7, 22, 0),
+                extendedProps: { slug: "connect-and-cheers" }
     },
     {
-        title: "Tichu Evening",
-        start: new Date(2025, 11, 23, 18, 30),
-        end: new Date(2025, 11, 23, 20, 30),
-        extendedProps: { slug: "tichu" }
+                title: "Tichu Evening",
+                start: new Date(2025, 11, 23, 18, 30),
+                end: new Date(2025, 11, 23, 20, 30),
+                extendedProps: { slug: "tichu" }
     },
     {
-        title: "Sound journey HANDPAN CONCERT",
-        start: new Date(2025, 11, 13, 18, 0),
-        end: new Date(2025, 11, 13, 19, 0),
-        extendedProps: { slug: "handpan-concert" }
+                title: "Sound journey HANDPAN CONCERT",
+                start: new Date(2025, 11, 13, 18, 0),
+                end: new Date(2025, 11, 13, 19, 0),
+                extendedProps: { slug: "handpan-concert" }
     },
     {
-        title: "Berndeutsch Konversationsgruppe",
-        start: new Date(2025, 11, 19, 19, 0),
-        end: new Date(2025, 11, 19, 21, 0),
-        extendedProps: { slug: "berndeutsch" }
+                title: "Berndeutsch Konversationsgruppe",
+                start: new Date(2025, 11, 20, 18, 0),
+                end: new Date(2025, 11, 20, 21, 0),
+                extendedProps: { slug: "berndeutsch" }
     },
     {
-        title: "Christmas Together in Bern",
-        start: new Date(2025, 11, 25, 19, 0),
-        end: new Date(2025, 11, 25, 22, 0),
-        extendedProps: { slug: "christmas-together" }
+                title: "Christmas Together in Bern",
+                start: new Date(2025, 11, 25, 19, 0),
+                end: new Date(2025, 11, 25, 22, 0),
+                extendedProps: { slug: "christmas-together" }
     }
-];
+    ];
